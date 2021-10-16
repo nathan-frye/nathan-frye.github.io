d5ff4530374aa82430a6052d299ef242c300da5e
nathan-frye <77402987+nathan-frye@users.noreply.github.com>
2021-10-16 01:09:00 -0400

    Initial Site Skeleton
    Very bare-bones html set up with chart.js - nonfunctional

---
+++ chart.js
@@ -0,0 +1,19 @@
+d3.json("data.json").then(function(dataset)
+{
+    var dimensions = {
+        width: 800,
+        height: 800,
+        margin: {
+            top: 10,
+            bottom: 50,
+            right: 10,
+            left: 50
+        }
+    }
+
+    var svg = d3.select("#linechart")
+                .style("width", dimensions.width)
+                .style("height", dimensions.height)
+    
+    var labels
+})
